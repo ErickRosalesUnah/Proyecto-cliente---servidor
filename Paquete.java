c5516b5841187070cfbefb50d7423c0e802b17a9
ErickRosalesUnah <50184746+ErickRosalesUnah@users.noreply.github.com>
2019-05-01 15:27:14 -0600

    Add files via upload

---
+++ Paquete.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clienteservidor2;
+
+import java.io.Serializable;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Erick Ayestas
+ */
+public class Paquete implements Serializable{
+    
+    String ipOrigen;
+    String ipDestino;
+    String usuario;
+    String mensaje;
+
+    public Paquete(String ipOrigen, String ipDestino, String usuario, String mensaje) {
+        this.ipOrigen = ipOrigen;
+        this.ipDestino = ipDestino;
+        this.usuario = usuario;
+        this.mensaje = mensaje;
+    }
+
+    public Paquete() {
+    }
+
+    public String getIpOrigen() {
+        return ipOrigen;
+    }
+
+    public void setIpOrigen(String ipOrigen) {
+        this.ipOrigen = ipOrigen;
+    }
+
+    public String getIpDestino() {
+        return ipDestino;
+    }
+
+    public void setIpDestino(String ipDestino) {
+        this.ipDestino = ipDestino;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+
